b6a5c72cbe309040202748485b3df07d4ebd2935
Seda <sedadalkiran0@gmail.com>
2022-12-19 17:51:40 +0300

    CameraManager Base

---
+++ Source/CameraManagerSystem/CMPlayerCameraManager.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Camera/PlayerCameraManager.h"
+#include "CMPlayerCameraManager.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CAMERAMANAGERSYSTEM_API ACMPlayerCameraManager : public APlayerCameraManager
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/CameraManagerSystem/CameraManagerSystem.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "CameraManagerSystem.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CameraManagerSystem, "CameraManagerSystem" );
